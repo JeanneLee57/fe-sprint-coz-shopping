b518f88a0b63a6b0c8eafa756e9d4a5aa5093a19
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-05-14 17:59:01 +0900

    북마크 기능 완성

--- src/components/BookmarkList.jsx
+++ src/components/BookmarkList.jsx
@@ -1,12 +1,35 @@
 import styles from "./BookmarkList.module.css";
+import Item from "./UI/Item";
 import Error from "./UI/Error";
+import { useState } from "react";
 
-function BookmarkList() {
+function BookmarkList({ bookmarkState, setBookmarkState }) {
+  const checkIsBookmarked = (item) => {
+    if (bookmarkState) {
+      return bookmarkState.some((x) => x.id === item.id);
+    }
+    return false;
+  };
   return (
-    <>
+    <div className={styles.mainbox}>
       <h1 className={styles.title}>북마크 리스트</h1>
-      <Error />
-    </>
+      <div className={styles.listWrapper}>
+        {bookmarkState && bookmarkState.length !== 0 ? (
+          bookmarkState
+            .slice(0, 4)
+            .map((item) => (
+              <Item
+                item={item}
+                isBookmarked={checkIsBookmarked(item)}
+                bookmarkState={bookmarkState}
+                setBookmarkState={setBookmarkState}
+              />
+            ))
+        ) : (
+          <Error />
+        )}
+      </div>
+    </div>
   );
 }
 
