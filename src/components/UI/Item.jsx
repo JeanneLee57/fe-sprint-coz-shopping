34069f536355bdc7be673daf7e84eeebf463b81e
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-05-16 11:31:26 +0900

    모달 기능 1차 완성

--- src/components/UI/Item.jsx
+++ src/components/UI/Item.jsx
@@ -1,9 +1,18 @@
 import styles from "./Item.module.css";
+import Modal from "./Modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Item = ({ item, setBookmarkState, isBookmarked }) => {
-  const handleBookmark = () => {
+  const [modalState, setModalState] = useState(false);
+  const handleModalOpen = () => {
+    setModalState(true);
+  };
+  const handleModalClose = () => {
+    setModalState(false);
+  };
+  const handleBookmark = (e, item) => {
     const bookmark = JSON.parse(localStorage.getItem("bookmark")) || [];
     const existingItemIndex = bookmark.findIndex((x) => x.id === item.id);
     const isExistingItem = existingItemIndex !== -1;
@@ -14,67 +23,86 @@ const Item = ({ item, setBookmarkState, isBookmarked }) => {
       bookmark.unshift(item);
     }
 
+    // localStorage.setItem("bookmark", JSON.stringify(bookmark));
+    // if (e.currentTarget === e.target && modalState) {
+    //   setBookmarkState(JSON.parse(localStorage.getItem("bookmark")));
+    // } else if (!modalState) {
+    //   setBookmarkState(JSON.parse(localStorage.getItem("bookmark")));
+    // }
     localStorage.setItem("bookmark", JSON.stringify(bookmark));
+    if (modalState) setModalState(false);
     setBookmarkState(JSON.parse(localStorage.getItem("bookmark")));
   };
 
   return (
-    <div className={styles.item}>
-      <div className={styles.imgBox}>
-        <img
-          className={styles.image}
-          src={item.image_url ? item.image_url : item.brand_image_url}
+    <>
+      {modalState && (
+        <Modal
+          imageUrl={item.image_url || item.brand_image_url}
+          handleModalClose={handleModalClose}
+          isBookmarked={isBookmarked}
+          handleBookmark={(e) => handleBookmark(e, item)}
+          title={item.title || item.brand_name}
         />
-        <FontAwesomeIcon
-          className={isBookmarked ? styles.yellowstar : styles.star}
-          size="lg"
-          icon={faStar}
-          onClick={() => {
-            handleBookmark();
-          }}
-        />
-      </div>
-      <div className={styles.detail}>
-        {
-          <h1 className={styles.title}>
-            {item.title ? item.title : item.brand_name}
-          </h1>
-        }
-        {(() => {
-          switch (item.type) {
-            case "Brand":
-              return <span className={styles.brand}>관심고객수</span>;
-            case "Product":
-              return (
-                <span className={styles.discount}>
-                  {item.discountPercentage}%
-                </span>
-              );
-            default:
-              return "";
+      )}
+      <div className={styles.item}>
+        <div className={styles.imgBox}>
+          <img
+            className={styles.image}
+            src={item.image_url ? item.image_url : item.brand_image_url}
+            onClick={handleModalOpen}
+          />
+          <FontAwesomeIcon
+            className={isBookmarked ? styles.yellowstar : styles.star}
+            size="lg"
+            icon={faStar}
+            onClick={(e) => {
+              handleBookmark(e, item);
+            }}
+          />
+        </div>
+        <div className={styles.detail}>
+          {
+            <h1 className={styles.title}>
+              {item.title ? item.title : item.brand_name}
+            </h1>
           }
-        })()}
-      </div>
-      <div className={styles.detail}>
-        <span>{item.sub_title ? item.sub_title : ""}</span>
-        <span className={styles.numbers}>
           {(() => {
             switch (item.type) {
-              case "Product":
-                return `${item.price
-                  .toString()
-                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원`;
               case "Brand":
-                return item.follower
-                  .toString()
-                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+                return <span className={styles.brand}>관심고객수</span>;
+              case "Product":
+                return (
+                  <span className={styles.discount}>
+                    {item.discountPercentage}%
+                  </span>
+                );
               default:
                 return "";
             }
           })()}
-        </span>
+        </div>
+        <div className={styles.detail}>
+          <span>{item.sub_title ? item.sub_title : ""}</span>
+          <span className={styles.numbers}>
+            {(() => {
+              switch (item.type) {
+                case "Product":
+                  return `${item.price
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원`;
+                case "Brand":
+                  return item.follower
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+                default:
+                  return "";
+              }
+            })()}
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
